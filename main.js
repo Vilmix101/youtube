f62a1b451362f83c0a3331a3ec7c50a647a17601
Vilmix101 <kirillserge35@gmail.com>
2025-05-21 19:01:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let dropdownList = document.querySelector(".dropdown__list")
+
+function changeList(){
+    dropdownList.classList.toggle("active")
+}
+
+let number = 1000 //числа
+let str = "Кирилл" //строки
+let bool = true
+
+number = 10 // 
+
+const num = 1000
+
+const logoText = document.querySelector(".content__title")
+const logoTexts = document.querySelectorAll(".content__title")
+console.log(logoText)
+
+// logoText.innerHTML = "ffff"
+logoText.style.color = "red"
+
+
+function changeText()
+{logoText.innerHTML = "ffff"}
